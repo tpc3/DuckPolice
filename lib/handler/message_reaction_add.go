c7bd3f961a4e7b1470908300160bfae3f8aaaa82
Clover_Midori <64039436+clover0916@users.noreply.github.com>
2023-03-17 21:58:49 +0000

    Fix panic

--- lib/handler/message_reaction_add.go
+++ lib/handler/message_reaction_add.go
@@ -7,9 +7,26 @@ import (
 )
 
 func MessageReactionAdd(session *discordgo.Session, reaction *discordgo.MessageReactionAdd) {
-	msg, _ := session.ChannelMessage(reaction.ChannelID, reaction.MessageID)
+	msg, _ := session.State.Message(reaction.ChannelID, reaction.MessageID)
+	if msg == nil {
+		msg, _ = session.ChannelMessage(reaction.ChannelID, reaction.MessageID)
+	}
 
-	if reaction.UserID != session.State.User.ID && (reaction.Emoji.ID == config.CurrentConfig.Duplicate.Delete || reaction.Emoji.Name == config.CurrentConfig.Duplicate.Delete) {
+	if reaction.Emoji.ID == "" && reaction.Emoji.Name == "" {
+		return
+	}
+
+	//メッセージについているリアクションを習得する
+	reactions, _ := session.MessageReactions(reaction.ChannelID, reaction.MessageID, config.CurrentConfig.Duplicate.Delete, 100, "", "")
+
+	reacted := false
+	for _, r := range reactions {
+		if r.ID == session.State.User.ID {
+			reacted = true
+		}
+	}
+
+	if reacted && reaction.UserID != session.State.User.ID && (reaction.Emoji.ID == config.CurrentConfig.Duplicate.Delete || reaction.Emoji.Name == config.CurrentConfig.Duplicate.Delete) {
 		go session.MessageReactionRemove(reaction.ChannelID, msg.MessageReference.MessageID, config.CurrentConfig.Duplicate.React, session.State.User.ID)
 		go session.ChannelMessageDelete(reaction.ChannelID, reaction.MessageID)
 	}
